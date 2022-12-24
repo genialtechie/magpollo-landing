19bcf40862da861b0f67ca04e795182f771cc1f7
genialtechie <thehaleembello@gmail.com>
2022-12-23 23:12:24 -0500

    fixed error

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -29,13 +29,14 @@ export default function Product({ product }) {
     const session = await response.json();
     window.location.href = session.url;
   }
+
   return (
     <div className="container h-fit grid grid-cols-1 lg:grid-cols-3">
       <div className="lg:col-span-2 relative">
         <div className="flex justify-center p-20 items-center bg-theme">
           <Image
-            src={product.image_url}
-            alt={product.name}
+            src={product?.image_url}
+            alt={product?.name}
             width={400}
             height={400}
             className="rounded-3xl scale-150 lg:scale-110"
@@ -43,16 +44,16 @@ export default function Product({ product }) {
         </div>
         <div className="absolute top-0 left-0 max-w-xl">
           <h3 className="text-3xl px-8 py-4 font-extrabold min-w-[200px] text-center bg-custom/70 text-white">
-            {product.name}
+            {product?.name}
           </h3>
           <p className="text-xl w-fit px-8 py-4 bg-custom/70 text-white">
-            {product.price} USD
+            {product?.price} USD
           </p>
         </div>
       </div>
       <div className="lg:col-span-1 px-10 py-5">
         <p className="mb-5 text-xl text-custom font-sans">
-          {product.description}
+          {product?.description}
         </p>
         <QtySelector
           min={1}
